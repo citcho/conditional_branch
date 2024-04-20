5623dcd1bb17c1a8e2ebefc7f9251b19e3a5c8a3
kobayashi <citcho716@gmail.com>
2024-04-20 12:35:54 +0900

    Step2修正

--- src/app/Http/Controllers/Step2Controller.php
+++ src/app/Http/Controllers/Step2Controller.php
@@ -4,19 +4,29 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Domain\Step2\Blizzard;
+use Domain\Step2\Fire;
+use Domain\Step2\MagicType;
 use Domain\Step2\Member;
 use Domain\Step2\Thunder;
+use FFI;
 
 class Step2Controller extends Controller
 {
     public function __invoke()
     {
+        $magics = [
+            MagicType::Fire => new Fire(),
+            MagicType::Blizzard => new Blizzard(),
+            MagicType::Thunder => new Thunder(),
+        ];
+
         // メンバーを作成
         $member = new Member(
             name: 'Sora',
         );
         // 魔法を作成
-        $magic = new Thunder();
+        $magic = $magics[MagicType::Fire];
 
         // 魔法を唱える
         $member->chant($magic);
